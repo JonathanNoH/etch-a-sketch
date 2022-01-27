c30a8420e736fa6909cd4d69215d807a27457ab9
JonathanNoH <jonathan897h@gmail.com>
2022-01-27 11:44:52 -0700

    add functionality to reset button

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function createGrid(size) {
-    if (size > 100) return;
     const divs = [];
     for (let i=0; i<size; i++) {
         for (let i=0; i<size; i++) {
@@ -19,19 +18,31 @@ function appendContainer(item) {
 }
 
 function resetBoard() {
-    return;
+    let userSize = prompt("Enter a size for new board. (Must be less than 100)", "");
+    while (!(userSize > 0 && userSize <= 100)) {
+        userSize = prompt("Error. Please enter a number between 0 and 100.", "")
+    }
+    let squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        container.removeChild(square);
+    })
+    createGrid(userSize);
+    addListeners();
+}
+
+function addListeners() {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach(child => {
+        child.addEventListener('mouseover', (e) => {
+            e.target.classList.add('hovered');
+        })
+    })
 }
 
 const container = document.querySelector(".container");
 createGrid(16);
+addListeners();
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach(child => {
-    child.addEventListener('mouseover', (e) => {
-        e.target.classList.add('hovered');
-    })
-})
 
 const resetButton = document.querySelector(".reset");
 
